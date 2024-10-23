1f61ca9f2656415b2df6f40afa0dc745d994db58
alexandraciurescu <alexandra.ciurescu@yahoo.com>
2024-10-23 13:35:22 +0300

    camera shake

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // Parametrii pentru shake
+    public float shakeDuration = 0.5f; // Durata shake-ului
+    public float shakeMagnitude = 0.1f; // Magnitudinea shake-ului
+
+    public IEnumerator Shake()
+    {
+        // Salvează poziția inițială a camerei
+        Vector3 originalPosition = transform.localPosition;
+        float elapsed = 0.0f;
+
+        // Efectul de shake
+        while (elapsed < shakeDuration)
+        {
+            // Generăm o mișcare mai fină
+            float x = Random.Range(-1f, 1f) * shakeMagnitude;
+            float y = Random.Range(-1f, 1f) * shakeMagnitude;
+
+            // Actualizăm poziția camerei
+            transform.localPosition = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
+
+            // Incrementăm timpul scurs
+            elapsed += Time.deltaTime;
+
+            // Așteptăm următorul frame
+            yield return null;
+        }
+
+        // Revenim la poziția originală
+        transform.localPosition = originalPosition;
+    }
+
+    // Funcție pentru a începe shake-ul
+    public void TriggerShake()
+    {
+        StartCoroutine(Shake());
+    }
+}
---
+++ Assets/Scripts/CollisionWithObstacles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CollisionWithObstacles : MonoBehaviour
+{
+    public CameraShake cameraShake; // Referință la CameraShake
+
+    void Start()
+    {
+        // Obține referința la CameraShake
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            // Pornim efectul de camera shake
+            cameraShake.TriggerShake();
+        }
+    }
+}
