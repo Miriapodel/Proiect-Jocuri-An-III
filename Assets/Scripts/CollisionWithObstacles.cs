fbbc164618e1ddd4ef3fe1d9f12fdb6c9d521b80
Stoinea Maria-Miruna <130232381+stoineamiruna@users.noreply.github.com>
2024-11-02 14:59:52 +0200

    Implementare vieti
    Taskul de creare vieti si spawnare

--- Assets/Scripts/CollisionWithObstacles.cs
+++ Assets/Scripts/CollisionWithObstacles.cs
@@ -6,19 +6,36 @@ public class CollisionWithObstacles : MonoBehaviour
     public CameraShake cameraShake; // Referință la CameraShake
     public PlayerMovement playerMovement;
 
+    public int lives = 3; // Numarul de vieti
+    public GameObject[] lifeObjects; // Obiectele de viață în scenă
+
     void Start()
     {
         // Obține referința la CameraShake
         cameraShake = Camera.main.GetComponent<CameraShake>();
         playerMovement = GetComponent<PlayerMovement>();
+
+        // Initializează UI - ul de vieți
+        UpdateLivesUI();
     }
 
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
+            // Reduce o viață
+            lives--;
+            UpdateLivesUI();
+
             // Pornim efectul de camera shake
             cameraShake.TriggerShake();
+
+            // Verifică dacă jucătorul a rămas fără vieți
+            if (lives <= 0)
+            {
+                //Game Over
+                Debug.Log("Game Over!");
+            }
         }
         if (collision.CompareTag("leftSide"))
         {
@@ -27,6 +44,15 @@ public class CollisionWithObstacles : MonoBehaviour
         if (collision.CompareTag("rightSide"))
         {
             playerMovement.ChangeLane(1);
+        }
+    }
+
+    void UpdateLivesUI()
+    {
+        for (int i = 0; i < lifeObjects.Length; i++)
+        {
+            // Activează sau dezactivează obiectele de viață în funcție de numărul de vieți rămase
+            lifeObjects[i].SetActive(i < lives);
         }
     }
 }
