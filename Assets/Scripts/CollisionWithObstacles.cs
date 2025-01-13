fdc7d4c93275ffa26caf3f94e6dc0765712a5161
Yanis Popescu <yanispavel.popescu@gmail.com>
2025-01-13 21:15:22 +0200

    Fine Tuning pentru obstacole

--- Assets/Scripts/CollisionWithObstacles.cs
+++ Assets/Scripts/CollisionWithObstacles.cs
@@ -112,7 +112,7 @@ public class CollisionWithObstacles : MonoBehaviour
             Vector3 directionToObstacle = (collision.transform.position - transform.position).normalized;
             float dotProduct = Vector3.Dot(transform.forward, directionToObstacle);
 
-            if (dotProduct > 0.925f) // Aproximativ frontal (1.0 ar fi direct frontal)
+            if (dotProduct > 0.915f) // Aproximativ frontal (1.0 ar fi direct frontal)
             {
                 // Coliziune frontală - pierde toate viețile
                 lives = 0;
